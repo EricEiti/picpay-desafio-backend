852445278118735b22fbf5dc643e43ed67569fdb
Eric Eiti Nomura <eric.nomura33@gmail.com>
2023-09-19 21:13:57 -0300

    Constructor User(UserDTO userDTO)

--- src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -1,6 +1,7 @@
 package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
+
 import com.picpaysimplificado.dtos.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
